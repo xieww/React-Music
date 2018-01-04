8b9f7bb05e2b4f96db4d6d52ff77fcd3a5bb4c73
lingfengxie <2801416900@qq.com>
2018-01-04 20:53:32 +0800

    ♨更♨新♨

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -6,6 +6,21 @@ import 'weui';
 import 'react-weui/build/packages/react-weui.css';
 
 class SearchPage extends Component {
+    state={
+        searchText: 'a',
+        results: []
+    };
+
+    handleChange(text, e){
+        let keywords = [text];
+        let results = SampleData.filter(/./.test.bind(new RegExp(keywords.join('|'),'i')));
+
+        if(results.length > 3) results = results.slice(0,3);
+        this.setState({
+            results,
+            searchText:text,
+        });
+    }
     render() {
         return (
             <div className="search_main">
