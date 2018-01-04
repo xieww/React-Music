94b38ecb108d81cf199a00f972a7b5f56c823abd
lingfengxie <2801416900@qq.com>
2018-01-04 21:26:39 +0800

    ♨更♨新♨

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { SearchBar } from "react-weui";
 //import styles
 import "weui";
 import "react-weui/build/packages/react-weui.css";
+import './SearchPage.less'
 
 class SearchPage extends Component {
   state = {
-    searchText: "a",
+    searchText: "",
     results: []
   };
 
   handleChange(text, e) {
     let keywords = [text];
-    let results = SampleData.filter(
-      /./.test.bind(new RegExp(keywords.join("|"), "i"))
-    );
+    // let results = SampleData.filter(
+    //   /./.test.bind(new RegExp(keywords.join("|"), "i"))
+    // );
 
-    if (results.length > 3) results = results.slice(0, 3);
+    // if (results.length > 3) results = results.slice(0, 3);
     this.setState({
-      results,
+    //   results,
       searchText: text
     });
   }
@@ -31,6 +32,20 @@ class SearchPage extends Component {
           defaultValue={this.state.searchText}
           placeholder="搜索歌曲、歌单、专辑"
         />
+        <Router>
+            <div className="hot_search">
+                <h3 className="hot_title">热门搜索</h3>
+                <div className="result_list">
+                    <Link className="tags" to="" query={{ id: 4 }}>梦想的声音第二季</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>DJ舞曲(华语)系列5 DJ</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>WHAT ARE WORDS</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>中国好声音 第六季</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>非酋 薛明媛/朱贺</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>一生所爱 卢冠廷</Link>
+                    <Link className="itemlist" to="" query={{ id: 4 }}>舍得 </Link>
+                </div>
+            </div>
+        </Router>
       </div>
     );
   }
