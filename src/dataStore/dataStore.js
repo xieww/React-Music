6ffe4778a51fc9ff3358235979fb1ba952032727
lingfengxie <2801416900@qq.com>
2018-01-16 15:30:58 +0800

    ❉新增❉

---
+++ src/dataStore/dataStore.js
@@ -0,0 +1,29 @@
+let slider = [
+      {
+        id: 13832,
+        linkUrl: "https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=3877825&g_f=shoujijiaodian",
+        picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M000001yhUnD2ADZvM.jpg"
+      },
+      {
+        id: 13851,
+        linkUrl: "https://c.y.qq.com/node/m/client/music_headline/index.html?_hidehd=1&_button=2&zid=530467",
+        picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M000001u5Mpz0FUm1i.jpg"
+      },
+      {
+        id: 11621,
+        linkUrl: "http://y.qq.com/w/album.html?albummid=003z9kma13yhja",
+        picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M000001Fin3W1w12as.jpg"
+      },
+      {
+        id: 13596,
+        linkUrl: "https://c.y.qq.com/node/m/client/music_headline/index.html?_hidehd=1&_button=2&zid=465481?ADTAG=jiaodiantu",
+        picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M0000031WCJk1l8MB6.jpg"
+      },
+      {
+        id: 13762,
+        linkUrl: "https://c.y.qq.com/node/m/client/music_headline/index.html?_hidehd=1&_button=2&zid=514746",
+        picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M0000023qiTv1D1g6d.jpg"
+      }
+]
+
+export  {slider};
