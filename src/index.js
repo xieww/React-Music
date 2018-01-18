843b1e59a0cc7aac06c74ecf8d15a78be83c77db
lingfengxie <2801416900@qq.com>
2018-01-18 14:28:30 +0800

    ♨更新♨

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route,} from "react-router-dom";
-import Recommend from './components/Recommend/Recommend';
-import RankingList from './components/RankingList/RankingList';
-import Search from './components/SearchPage/SearchPage';
+
 
 ReactDOM.render(
-    (<Router>
-        <div>
-        <Route  eexact path="/" component={App}/>
-        <Route  path="/recommend" component={Recommend}/>
-        <Route  path="/rankingList" component={RankingList}/>
-        <Route  path="/search" component={Search}/>
-        </div>
-    </Router>), document.getElementById('root')
+    <App/>, document.getElementById('root')
 );
 registerServiceWorker();
